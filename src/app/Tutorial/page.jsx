a8827461140c0a8efd33eb5b19a782e7c770ff8b
kinshuk bageja <kinshukbageja@gmail.com>
2025-05-04 21:36:08 +0530

    updated Tutorial Link

--- src/app/Tutorial/page.jsx
+++ src/app/Tutorial/page.jsx
@@ -16,9 +16,8 @@ function Tutorial() {
         {/* Embed YouTube Video */}
         <iframe
           className="w-full h-full object-cover"
-          src="https://www.youtube.com/embed/f5EidWD1llA"
+          src="https://www.youtube.com/embed/WPqGylce3r8"
           title="YouTube video player"
-          frameBorder="0"
           allow="accelerometer; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
           allowFullScreen
         ></iframe>
